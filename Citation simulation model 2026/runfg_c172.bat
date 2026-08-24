b9906f66856e5afd4d5d89ea08e3c8cfe992c5bd
erietman46 <e.rietman@student.tudelft.nl>
2026-08-24 19:41:51 +0200

    commit

--- Citation simulation model 2026/runfg_c172.bat
+++ Citation simulation model 2026/runfg_c172.bat
@@ -1,5 +1,8 @@
-C:
-cd C:\Apps\FlightGear2020.3
+@echo off
 
-SET FG_ROOT=C:\Apps\FlightGear2020.3\data
-.\\bin\fgfs --aircraft=c172p --fdm=network,localhost,5501,5502,5503 --fog-fastest --disable-clouds --start-date-lat=2004:06:01:09:00:00 --disable-sound --on-ground --enable-freeze --airport=KSFO --runway=10L --altitude=0 --heading=113 --offset-distance=0 --offset-azimuth=0
+set "FG_HOME=C:\Program Files\FlightGear 2024.1"
+set "FG_ROOT=%FG_HOME%\data"
+
+cd /d "%FG_HOME%"
+
+".\bin\fgfs.exe" --aircraft=c172p --fdm=network,localhost,5501,5502,5503 --fog-fastest --disable-clouds --start-date-lat=2004:06:01:09:00:00 --disable-sound --on-ground --enable-freeze --airport=KSFO --runway=10L --altitude=0 --heading=113 --offset-distance=0 --offset-azimuth=0
