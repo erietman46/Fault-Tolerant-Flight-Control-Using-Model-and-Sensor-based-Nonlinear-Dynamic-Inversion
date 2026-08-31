9dae6f5fcf3f6e65df27d57a066f20722f0ea01e
erietman46 <e.rietman@student.tudelft.nl>
2026-08-31 11:03:19 +0200

    delete unnecessary files

--- Citation simulation model 2026/EXAMPLE_runfg_c172.bat
+++
@@ -1,5 +0,0 @@
-C:
-cd C:\Program Files\FlightGear 2024.1 
-
-SET FG_ROOT=C:\Program Files\FlightGear 2024.1\data
-.\\bin\fgfs --aircraft=c172p --fdm=network,localhost,5501,5502,5503 --fog-fastest --disable-clouds --start-date-lat=2004:06:01:09:00:00 --disable-sound --on-ground --enable-freeze --airport=KSFO --runway=10L --altitude=0 --heading=113 --offset-distance=0 --offset-azimuth=0
